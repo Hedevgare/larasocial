9df1abe32b5b43e76056211335965516f7430092
Edgar Moreira <hedegaremoreira@gmail.com>
2023-01-20 20:20:14 +0000

    Add User and Post seeders

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'message' => fake()->text(),
+            'created_at' => fake()->dateTimeBetween('-1 week', 'now'),
+        ];
+    }
+}
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory()->count(10)->state(new Sequence(
+            fn ($sequence) => ['user_id' => User::all()->random()->id]
+        ))->create();
+    }
+}
