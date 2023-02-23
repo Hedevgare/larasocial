a72fde9ebc1392cf70bb4ee78812c10cd6a54717
Edgar Moreira <hedegaremoreira@gmail.com>
2023-02-23 18:03:11 +0000

    Changed home page follow user buttons

--- resources/js/Components/FollowUser.jsx
+++ resources/js/Components/FollowUser.jsx
@@ -1,14 +1,13 @@
 import { useForm } from '@inertiajs/react';
 import axios from 'axios';
 import React, { useState } from 'react';
-import SecondaryButton from './SecondaryButton';
 
 export default function FollowUser({ user }) {
-    const { data, processing } = useForm({
+    const { data } = useForm({
         user_id: user.id
     });
-    
-    const [ isFollowing, setIsFollowing ] = useState(false);
+
+    const [isFollowing, setIsFollowing] = useState(false);
 
     const follow = () => {
         axios.post(route('follow'), data)
@@ -16,13 +15,18 @@ export default function FollowUser({ user }) {
     }
 
     return (
-        <div key={user.id} className="flex items-center mt-6">
+        <div key={user.id} className="flex justify-between items-center mt-6">
             <img className="w-[50px] rounded-full mr-6" src={user.profile_photo} />
-            <div className="truncate grow">
+            <div className="truncate grow mr-5">
                 {user.name}
             </div>
-            <div className="row-end-1">
-                {!isFollowing && <SecondaryButton onClick={follow} processing={processing}>+</SecondaryButton>}
+            <div className={"inline-flex h-[40px] min-w-[40px] rounded-full items-center justify-center cursor-pointer " + (isFollowing ? "border" : "bg-indigo-700 text-white")} onClick={follow}>
+                {!isFollowing ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                </svg>
+                }
             </div>
         </div>
     );
