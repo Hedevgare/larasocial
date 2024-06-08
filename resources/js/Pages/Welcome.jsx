f746ae1cd4034976653440191015385abe75ae19
Edgar Moreira <hedegaremoreira@gmail.com>
2024-06-08 22:38:30 +0100

    Fix Welcome page mobile layout

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,35 +5,14 @@ export default function Welcome(props) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                {/* <div className="fixed top-0 right-0 px-6 py-4 sm:block">
-                    {props.auth.user ? (
-                        <Link href={route('home')} className="text-sm text-gray-700 dark:text-gray-500 underline">
-                            Home
-                        </Link>
-                    ) : (
-                        <>
-                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
-                                Log in
-                            </Link>
-
-                            <Link
-                                href={route('register')}
-                                className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
-                </div> */}
-
-                <div className='flex-row'>
-                    <div className='flex items-end'>
+            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 items-center sm:pt-0">
+                <div className='px-4 text-center'>
+                    <div className='flex flex-col items-center lg:flex-row lg:items-end'>
                         <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                        <p className='text-6xl text-white ml-4'>Larasocial</p>
+                        <p className='text-4xl lg:text-6xl text-white lg:ml-4'>Larasocial</p>
                     </div>
                     <p className='text-white py-4'>Connect with your friends and the world around you.</p>
-                    <div className="py-6 text-center">
+                    <div className="py-6">
                         {props.auth.user ? (
                             <Link href={route('home')} className="text-sm text-white bg-indigo-700 px-8 py-2 rounded">
                                 Continue
